c87b539ba2601e712d86ede833fb5df377ab7b4e
Harri Peltola <harri.peltola@qvik.fi>
2020-06-16 12:44:46 +0300

    initial commit

---
+++ popup.d.ts
@@ -0,0 +1,10 @@
+import { FunctionalComponent } from 'preact';
+import './popup.scss';
+export interface IPopupProps {
+    message?: string;
+    btnYesTitle?: string;
+    btnNoTitle?: string;
+    redirectUrl: string;
+    timeout: number;
+}
+export declare const Popup: FunctionalComponent<IPopupProps>;
